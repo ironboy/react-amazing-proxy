2fbd2c1a0c6e1a2f1707fbd09239af2f2cf06532
Thomas Frank <thomasfrank@Thomass-MacBook-2.local>
2019-11-23 19:08:02 +0100

    default to build when run programmatically

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 module.exports = (settings) => {
   if (typeof settings !== 'object') { settings = {}; }
+  if (settings.dev === undefined) { settings.dev = false; }
   global.__reactAmazingProxySettings = settings;
   require('./start');
 }
